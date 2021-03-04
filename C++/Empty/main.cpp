0973d7791ede686fa7fff510bd3eef6c41389edf
Ilya Rosenberg <mr.ilya.pavlov@gmail.com>
2021-03-04 22:35:47 +0300

    Triangle C++

--- C++/Empty/main.cpp
+++ C++/Empty/main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+class Triangle {
+private:
+    int a=0,b=0,c=0;
+public:
+    Triangle(int v1,int v2,int v3){
+        a=v1;
+        b=v2;
+        c=v3;
+    };
+
+    ~Triangle(){
+        a=0;
+        b=0;
+        c=0;
+    };
+
+    void perimetr() {
+        int p=0;
+        p=a+b+c;
+        cout << "P=" <<p << endl;
+    }
+
+    void square() {
+        int p=(a+b+c)/2;
+        int S=sqrt(p*(p-a)*(p-b)*(p-c));
+        cout << "S=" << S << endl;
+    }
+};
 
 
 int main() {
-
-
-    cout << "Hello, World!" << endl;
+    Triangle obj (4, 3, 5);
+    obj.perimetr();
+    obj.square();
     return 0;
 }
